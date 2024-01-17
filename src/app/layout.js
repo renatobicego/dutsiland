2c8c29661bfabc40b825c23e5da095a65506d8be
renatobicego <renatobicego@gmail.com>
2024-01-17 17:30:46 -0300

    Metadata update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,76 @@
-import { Piazzolla } from 'next/font/google'
-import './globals.css'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import { Providers } from './providers'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-const piazzolla = Piazzolla({ 
-  subsets: ['latin'], 
-  display: 'swap',
-  weight: 'variable'
-})
+import { Piazzolla } from "next/font/google";
+import "./globals.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Providers } from "./providers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+const piazzolla = Piazzolla({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "variable",
+});
 
 export const metadata = {
-  title: 'Estudio Dutsiland',
-  description: 'Tu faro de la navegación web',
-}
+  title: "Estudio Dutsiland",
+  description: "Construimos experiencias web sobresalientes.",
+  keywords: [
+    "Next.js",
+    "dutsiland",
+    "desarrollador",
+    "estudio web",
+    "web",
+    "empresa",
+  ],
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Estudio Dutsiland",
+    url: "https://dutsiland.com/",
+    siteName: "Estudio Dutsiland",
+    locale: "es_ES",
+    type: "website",
+    description: "Construimos experiencias web sobresalientes.",
+    images: "/opengraph-image.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Estudio Dutsiland",
+    description: "Construimos experiencias web sobresalientes.",
+    images: ["/opengraph-image.png"], // Must be an absolute URL
+  },
+  robots: {
+    index: true,
+    follow: true,
+    "max-image-preview": "large",
+    "max-snippet": -1,
+  },
+};
 
 export default function RootLayout({ children }) {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    "@id": "https://dutsiland.com/",
+    name: "Estudio Dutsiland",
+    image: "/opengraph-image.png",
+    description:
+      "Construimos experiencias web sobresalientes.",
+  };
   return (
-    <html lang="en" className='scroll-smooth'>
+    <html lang="en" className="scroll-smooth">
       <body className={piazzolla.className}>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
         <Providers>
-          <Header  />
+          <Header />
           {children}
           <Footer />
         </Providers>
         <SpeedInsights />
       </body>
     </html>
-  )
+  );
 }
